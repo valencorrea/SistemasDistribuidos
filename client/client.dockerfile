8853651e5ae9a870194a53d776526d4ce7e9e832
Val <vcorrea@fi.uba.ar>
2025-04-17 16:19:02 -0300

    queue and dequeue from a file

--- client/client.dockerfile
+++ client/client.dockerfile
@@ -2,6 +2,7 @@ FROM python:3.9-slim
 COPY client/client.py /root/client/client.py
 COPY consumer/consumer.py /root/consumer/consumer.py
 COPY producer/producer.py /root/producer/producer.py
+COPY files/movies.txt /root/files/movies.txt
 RUN pip install pika
 ENV PYTHONPATH="/root"
 CMD ["python", "/root/client/client.py"]
--- client/client.py
+++ client/client.py
@@ -17,16 +17,17 @@ if __name__ == '__main__':
     time.sleep(10) # espera que rabbit termine de conectarse
     client = Client()
 
-    #file = open("../files/movies.txt", "r")
-    #lines = file.readlines()
-    #file.close()
+    file = open("root/files/movies.txt", "r")
+    lines = file.readlines()
+    file.close()
 
-    result = client.send({"cola": "mensaje de prueba"})
+    for line in lines:
+        result = client.send({"cola": line})
 
-    if result:
-        print(f"[MAIN] Mensaje recibido correctamente: {result}")
-    else:
-        print("[MAIN] No se recibió ningún mensaje")
+        if result:
+            print(f"[MAIN] Mensaje recibido correctamente: {result}")
+        else:
+            print("[MAIN] No se recibió ningún mensaje")
 
     time.sleep(60)
 
